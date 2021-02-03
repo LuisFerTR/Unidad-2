faf574589f985decd5fe911fa64a3fbace5cf06b
Luis Talavera <l19141205@queretaro.tecnm.mx>
2021-02-03 08:07:02 -0600

    Agregar archivos de proyecto.

---
+++ Archivos/Lexico0.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Archivos
+{
+    class Lexico0
+    {
+        private StreamReader archivo;
+        private StreamWriter bitacora;
+
+        public Lexico0()
+        { 
+            Console.WriteLine("Compilando prueba.txt");
+
+            try
+            {
+                if (File.Exists("C:\\Archivos\\prueba.txt"))
+                {
+                    archivo = new StreamReader("C:\\Archivos\\prueba.txt");
+                    bitacora = new StreamWriter("C:\\Archivos\\prueba.log");
+                    bitacora.AutoFlush = true;
+
+                    bitacora.WriteLine("Archivo: prueba.txt");
+                    bitacora.WriteLine("Directorio: C:\\Archivos");
+                }
+                else
+                {
+                    throw new FileNotFoundException();
+                }
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        ~Lexico0()
+        {
+            Console.WriteLine("Finaliza compilacion de prueba.txt");
+            CerrarArchivos();
+            Console.ReadKey();
+        }
+
+        private void CerrarArchivos()
+        {
+            archivo.Close();
+            bitacora.Close();
+        }
+
+        public void Display()
+        {
+            if (archivo != null)
+            {
+                while (!archivo.EndOfStream)
+                {
+                    Console.Write((char)archivo.Read());
+                }
+            }
+        }
+    }
+}
---
+++ Archivos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Archivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                
+                Lexico0 l = new Lexico0();
+                l.Display();
+                int x = 0;
+                x = 10;
+            }
+            // x = 20;
+        }
+    }
+}
